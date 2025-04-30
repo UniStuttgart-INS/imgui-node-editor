441085f8757688e819ea75de0ac0aed87f72ccd9
ToppDev <dev@topp.cc>
2025-04-30 09:33:20 +0200

    Expose Canvas for zooming

--- imgui_node_editor_internal.h
+++ imgui_node_editor_internal.h
@@ -1324,6 +1324,7 @@ struct EditorContext
     ContextMenuAction& GetContextMenu() { return m_ContextMenuAction; }
     ShortcutAction& GetShortcut() { return m_ShortcutAction; }
 
+    const ImGuiEx::Canvas& GetCanvas() const { return m_Canvas; }
     const ImGuiEx::CanvasView& GetView() const { return m_Canvas.View(); }
     const ImRect& GetViewRect() const { return m_Canvas.ViewRect(); }
     const ImRect& GetRect() const { return m_Canvas.Rect(); }
